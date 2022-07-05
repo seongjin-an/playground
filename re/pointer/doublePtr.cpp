79ab9442f111ac774578c62e0762494265aa5288
seongjin-an <seongjin4us@gmail.com>
2022-07-06 00:25:03 +0900

    double ptr

--- re/pointer/doublePtr.cpp
+++ re/pointer/doublePtr.cpp
@@ -22,11 +22,28 @@ void func3(int argc, char (*argv)[5]){
     }
 }
 
+void fun(char *sss){
+    std::cout << "sss: " << (sss) << std::endl;
+}
+
+void fun2(char sss[]){
+    std::cout << "sss: " << (sss) << std::endl;
+}
+
 int main(int argc, char *argv[]){
     char strs[][5] = {"abcd", "efgh"};
+    char sss[5] = "ansj";
+//    char s2[] = "s2s2";
+    char s2[] = {'s', '2', 's', '2', '\0'};
+    std::cout << "hi " << s2 << std::endl;
+    fun(sss);
+    fun(sss);
+    fun(s2);
     func(argc, argv);
     func2(argc, argv);
     func3(argc, strs);
+    int i[] = {1,2,3};
+    std::cout << i << std::endl;
 }
 /*
  * g++ imsi.cpp -o imsi
