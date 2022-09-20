14027839ed42dd98c79b8fdc94b7231bddc9c4f5
seongjin-an <seongjin4us@gmail.com>
2022-09-20 09:31:10 +0900

    vec

--- fun/dstrc/vector.cpp
+++ fun/dstrc/vector.cpp
@@ -4,12 +4,40 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 void f(std::vector<int> &v);
 void f(std::vector<int> v[10]);
 void f(std::vector<std::vector<int>> &v);
 
 int main() {
+    std::vector<int> v0;
+    for(int i = 0; i < 10; i++) v0.push_back(i);
+    for(int a : v0) std::cout << a << " ";
+    std::cout << '\n';
+    v0.pop_back();
+
+    for(int a : v0) std::cout << a << " ";
+    std::cout << '\n';
+
+    v0.erase(v0.begin(), v0.begin() + 1);
+
+    for(int a : v0) std::cout << a << " ";
+    std::cout << '\n';
+
+    auto a = std::find(v0.begin(), v0.end(), 100);
+    if(a == v0.end()) std::cout << "not found!!" << '\n';
+
+    std::fill(v0.begin(), v0.end(), 10);
+    for(int a : v0) std::cout << a << " ";
+    std::cout << '\n';
+    v0.clear();
+    std::cout << "EMPTY??\n";
+    for(int a : v0) std::cout << a << " ";
+    std::cout << '\n';
+
+    std::cout << "\n/////////////////////////////////////" << '\n';
+
     std::vector<int> v;
     for (int i = 1; i <= 3; i++) v.push_back(i);
     f(v);
