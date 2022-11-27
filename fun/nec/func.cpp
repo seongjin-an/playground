a8d0832bed621b96e058dd97cda19705f5cd6341
seongjin-an <seongjin4us@gmail.com>
2022-11-28 00:57:54 +0900

    객체지향

---
+++ fun/nec/func.cpp
@@ -0,0 +1,39 @@
+//
+// Created by seongjin on 22. 11. 28.
+//
+
+#include <iostream>
+
+struct Person {
+private:
+    std::string name;
+public:
+    Person() {
+        std::cout << "Person is constructed" << '\n';
+    }
+    void setName(std::string _name) {
+        this->name = _name;
+    }
+    std::string getName() {
+        return this->name;
+    }
+    ~Person() {
+        std::cout << name << " object is destructed..." << '\n';
+    }
+};
+
+Person * givenPerson(std::string name) {
+    Person *person = new Person;
+    person->setName(name);
+    return person;
+}
+
+int main() {
+    Person *person = givenPerson("ansj");
+    std::cout << person << '\n';
+    std::cout << "name: " << person->getName() << '\n';
+
+    std::cout << "before delete!" << '\n';
+    delete person;
+    std::cout << "after delete!" << '\n';
+}
