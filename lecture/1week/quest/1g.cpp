e16aeb90be14c6953bd506e180dc76fe7d5691f4
seongjin-an <seongjin4us@gmail.com>
2022-12-26 00:07:50 +0900

    백준 9996

--- lecture/1week/quest/1g.cpp
+++ lecture/1week/quest/1g.cpp
@@ -5,27 +5,23 @@ using namespace std;
 
 int n;
 string pattern;
-string str;
 int main() {
     cin >> n;
     cin >> pattern;
-    // string::iterator beginPattern = pattern.begin();
-    // string::iterator endPattern = pattern.end() - 1;
-    char beginPattern = pattern.at(0);
-    char endPattern = pattern.at(pattern.size() - 1);
+    string arr[n];
     for (int i = 0; i < n; i++) {
+        string str;
         cin >> str;
-        // string::iterator beginIter = str.begin();
-        // string::iterator endIter = str.end() - 1;
-        char beginStr = str.at(0);
-        char endStr = str.at(str.size() - 1);
-        if (beginPattern == beginStr && endPattern == endStr) {
+        arr[i] = str;
+    }
+
+    for (int i = 0; i < n; i++) {
+        string str = arr[i];
+        if (*(pattern.begin()) == *(str.begin()) && *(pattern.end() - 1) == *(str.end() - 1)){
             cout << "DA" << '\n';
         } else {
             cout << "NE" << '\n';
         }
     }
-    
-
     return 0;
 }
