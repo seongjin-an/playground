7a7ef592d9eab78ccbc7ad68b507d682f8711047
seongjin-an <seongjin4us@gmail.com>
2022-12-26 00:18:57 +0900

    백준 9996

--- lecture/1week/quest/1g.cpp
+++ lecture/1week/quest/1g.cpp
@@ -15,9 +15,17 @@ int main() {
         arr[i] = str;
     }
 
+    size_t idx = pattern.find("*");
+    string partofFirst = pattern.substr(0, idx);
+    size_t firstLen = partofFirst.size();
+    string partofLast = pattern.substr(idx + 1, pattern.size());
+    size_t lastLen = partofLast.size();
+
     for (int i = 0; i < n; i++) {
         string str = arr[i];
-        if (*(pattern.begin()) == *(str.begin()) && *(pattern.end() - 1) == *(str.end() - 1)){
+        string first = str.substr(0, firstLen);
+        string last = str.substr(str.size() - lastLen, str.size());
+        if (partofFirst == first && partofLast == last){
             cout << "DA" << '\n';
         } else {
             cout << "NE" << '\n';
