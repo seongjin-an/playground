da3052be33804f61f8932b007bce4b5ba18dde2c
seongjin-an <seongjin4us@gmail.com>
2022-12-30 23:55:11 +0900

    백준1620

---
+++ lecture/1week/quest/1i.cpp
@@ -0,0 +1,34 @@
+// 1620
+// https://www.acmicpc.net/problem/1620
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int n, m;
+map<string, int> pocke;
+string temp;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    string arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        pocke.insert({temp, i + 1});
+        arr[i] = temp;
+    }
+    for (int j = 0; j < m; j++) {
+        cin >> temp;
+        int res = stoi(temp);
+        if (res != 0) { // 숫자
+            cout << arr[res - 1] << '\n';
+        } else { // 문자
+            cout << res << '\n';
+        }
+    }
+    return 0;
+}
---
+++ lecture/1week/quest/1i2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int n, m;
+string s;
+map<string, int> mp;
+map<int, string> mp2;
+string a[100004];
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        mp[s] = i + 1;
+        mp2[i + 1] = s;
+        a[i + 1] = s;
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> s;
+        if (atoi(s.c_str()) == 0) {
+            cout << mp[s] << '\n';
+        } else {
+            cout << a[atoi(s.c_str())] << '\n';
+        }
+    }
+    return 0;
+}
