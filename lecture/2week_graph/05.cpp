5fd4ef0245e068fd460f47c6c2a93ae785760f2c
seongjin-an <seongjin4us@gmail.com>
2023-01-12 21:53:09 +0900

    인접리스트

--- lecture/2week_graph/05.cpp
+++ lecture/2week_graph/05.cpp
@@ -9,3 +9,34 @@
 2번.
 0번부터 방문안한 노드를 찾고 해당 노드부터 방문, 연결된 노드를 이어서 방문해서 출력하는 재귀함수를 만들고 싶다면 어떻게 해야할 까? 또한, 정점을 방문하고 다시 방문하지 않게 만드려면 어떻게 해야할까?
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int V = 10;
+vector<int> adj[V];
+int visited[V];
+void go(int idx) {
+    cout << idx << '\n';
+    visited[idx] = 1;
+    for (int there : adj[idx]) {
+        if (visited[there]) continue;
+        go(there);
+    }
+    return ;
+}
+
+int main() {
+    adj[1].push_back(2);
+    adj[2].push_back(1);
+
+    adj[1].push_back(3);
+    adj[3].push_back(1);
+
+    adj[3].push_back(4);
+    adj[4].push_back(3);
+    for (int i = 0; i < V; i++) {
+        if (adj[i].size() && visited[i] == 0) go(i);
+    }
+}
