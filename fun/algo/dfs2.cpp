0a854dc2c3bed64837c2cd03286e9f23beab7eed
seongjin-an <seongjin4us@gmail.com>
2023-01-20 23:17:12 +0900

    dfs!!

--- fun/algo/dfs2.cpp
+++ fun/algo/dfs2.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
 const int count = 8;
 vector<int> graph[count];
 int visited[count];
-stack<int> st;
+
+void dfs(int u) {
+    visited[u] = 1;
+    cout << u << ' ';
+    for (int v : graph[u]) {
+        if (visited[v] == 0) {
+            dfs(v);
+        }
+    }
+    return;
+}
+
 int main() {
     graph[0].push_back(1); graph[1].push_back(0);
     graph[0].push_back(2); graph[2].push_back(0);
     graph[1].push_back(3); graph[3].push_back(1);
     graph[1].push_back(4); graph[4].push_back(1);
     graph[2].push_back(5); graph[5].push_back(2);
-    graph[2].push_back(6); graph[2].push_back(6);
+    graph[2].push_back(6); graph[6].push_back(2);
     graph[4].push_back(5); graph[5].push_back(4);
     graph[3].push_back(7); graph[7].push_back(3);
-
-    st.push(0);
-    visited[0] = 1;
-    while(st.size() != 0) {
-        int node = st.top(); st.pop();
-        cout << node << ' ';
-        // visited[node] = 1;
-        for (int v : graph[node]) {
-            if (visited[v] == 0) {
-                st.push(v);
-                visited[v] = 1;
-            }
-        }
-    }
-
+    dfs(0);
     return 0;
 }
---
+++ fun/algo/dfs3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int m, n, k, y, x, ret, ny, nx, t;
+int a[104][104];
+bool visited[104][104];
+
+void dfs(int y, int x) {
+    visited[y][x] = 1;
+    for (int i = 0; i < 4; i++) {
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+        if (a[ny][nx] == 1 && !visited[ny][nx]) {
+            dfs(ny, nx);
+        }
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >>a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == 1 && !visited[i][j]) {
+                ret++; dfs(i, j);
+            }
+        }
+    }
+    cout << ret << '\n';
+    return 0;
+}
