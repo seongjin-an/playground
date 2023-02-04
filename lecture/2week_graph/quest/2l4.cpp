929c2fa2cc0bd1896d29132ae4aec60d40eb0d64
seongjin-an <seongjin4us@gmail.com>
2023-02-04 22:04:02 +0900

    백준 2852

--- lecture/2week_graph/quest/2l4.cpp
+++ lecture/2week_graph/quest/2l4.cpp
@@ -5,10 +5,19 @@
 using namespace std;
 
 int n, t, mm, ss, a, asum, b, bsum, c, pre;
+string curtime;
+string print(int a) {
+    string d = "00" + to_string(a / 60);
+    string e = "00" + to_string(a % 60);
+    return d.substr(d.size() - 2, 2) + ":" + e.substr(e.size() - 2, 2);
+}
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
     for (int i = 0; i < n; i++) {
-        scanf("%d %d:%d", &t, &mm, &ss);
+        cin >> t >> curtime;
+        mm = atoi(curtime.substr(0, 2).c_str());
+        ss = atoi(curtime.substr(3, 2).c_str());
         c = mm * 60 + ss;
         if (a > b) {
             asum += c - pre;
@@ -28,7 +37,7 @@ int main() {
         }
         
     }
-    printf("%02d:%02d\n", asum / 60, asum % 60);
-    printf("%02d:%02d", bsum / 60, bsum % 60);
+    cout << print(asum) << '\n';
+    cout << print(bsum) << '\n';
     return 0;
 }
