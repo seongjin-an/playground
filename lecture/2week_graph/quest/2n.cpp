77b2f125e3a66c6723accb9b5590937d3b27c2f4
seongjin-an <seongjin4us@gmail.com>
2023-02-06 02:24:23 +0900

    1436

--- lecture/2week_graph/quest/2n.cpp
+++ lecture/2week_graph/quest/2n.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-void go(int * arr, int * dish, int idx, int depth, int size) {
-    if (idx == depth) {
-        for (int i = 0; i < depth; i++) {
+void go(int * arr, int * dish, int r, int idx, int size) {
+    if (idx == r) {
+        for (int i = 0; i < r; i++) {
             cout << *(dish + i);
         }
         cout << '\n';
@@ -12,7 +12,7 @@ void go(int * arr, int * dish, int idx, int depth, int size) {
     }
     for (int i = 0; i < size; i++) {
         *(dish + idx) = *(arr + i);
-        go(arr, dish, idx + 1, depth, size);
+        go(arr, dish, r, idx + 1, size);
     }
 }
 int main() {
@@ -20,6 +20,6 @@ int main() {
     cin >> n;
     int arr[] = {0,1,2,3,4,5,6,7,8,9};
     int dish[n] = {0};
-    go(arr, dish, 0, n, 10);
+    go(arr, dish, n, 0, 10);
     return 0;
 }
