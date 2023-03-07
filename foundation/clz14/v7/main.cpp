e6cc5b85d91925c5beec1975b1decc2c048f6e57
seongjin-an <seongjin4us@gmail.com>
2023-03-07 22:56:13 +0900

    template non-type argument(expression argument)

---
+++ foundation/clz14/v7/main.cpp
@@ -0,0 +1,30 @@
+#include "arraytp.h"
+#include <iostream>
+
+int main() {
+    using std::cout;
+    ArrayTP<int, 10> sums;
+    ArrayTP<double, 10> aves;
+    ArrayTP<ArrayTP<int, 5>, 10> twodee;
+
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        sums[i] = 0;
+        for (j = 0; j <5; j++) {
+            twodee[i][j] = (i + 1) *(j + 1);
+            sums[i] += twodee[i][j];
+        }
+        aves[i] = (double) sums[i] / 10;
+    }
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 5; j++) {
+            cout.width(2);
+            cout << twodee[i][j] << ' ';
+        }
+        cout << ": 합계 = ";
+        cout.width(3);
+        cout << sums[i] << ", 평균 = " << aves[i] << '\n';
+    }
+    cout << "프로그램을 종료합니다.\n";
+    return 0;
+}
