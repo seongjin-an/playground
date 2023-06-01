6ea46f2ddfc279a5e21ceb6478f5a0efc766bb16
seongjin-an <seongjin4us@gmail.com>
2023-06-02 01:48:30 +0900

    linked list

--- DataStructure/List/LinkedList/LinkedList.c
+++ DataStructure/List/LinkedList/LinkedList.c
@@ -26,9 +26,50 @@ void SLL_DestroyNode(Node* Node) {
     free(Node);
 }
 
+void SLL_AppendNode(Node** Head, Node* NewNode) {
+    if ((*Head) == NULL) {
+        *Head = NewNode;
+    } else {
+        Node* Tail = (*Head);
+        while (Tail->NextNode != NULL) {
+            Tail = Tail->NextNode;
+        }
+        Tail->NextNode = NewNode;
+    }
+}
+
+void print(Node** Head) {
+    // do {
+    //     ElementType el = (*Head)->Data;
+    //     printf("elementType: %d\n", el);
+    //     Head = &((*Head)->NextNode);
+    // } while (*Head != NULL);
+    do {
+        ElementType el = (*Head)->Data;
+        printf("elementType: %d\n", el);
+        Head = &((*Head)->NextNode);
+    } while ((*Head) != NULL);
+}
+
 int main() {
     Node* MyNode = SLL_CreateNode(117);
-    printf("%d\n", MyNode->Data);
     free(MyNode);
+
+    Node* List = NULL;
+    Node* NewNode = NULL;
+    NewNode = SLL_CreateNode(117);
+    SLL_AppendNode(&List, NewNode);
+    NewNode = SLL_CreateNode(119);
+    SLL_AppendNode(&List, NewNode);
+    NewNode = SLL_CreateNode(121);
+    SLL_AppendNode(&List, NewNode);
+
+    print(&List);
+    while (List->NextNode != NULL) {
+        NewNode = List;
+        List = List->NextNode;
+        free(NewNode);
+    }
+    free(List);
     return 0;
 }
