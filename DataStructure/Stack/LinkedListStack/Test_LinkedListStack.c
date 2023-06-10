deb47765e79de4b58efbe87956c2a44e1d08842b
seongjin-an <seongjin4us@gmail.com>
2023-06-10 15:30:42 +0900

    linked list stack

---
+++ DataStructure/Stack/LinkedListStack/Test_LinkedListStack.c
@@ -0,0 +1,36 @@
+#include "LinkedListStack.h"
+
+int main(void)
+{
+    int i = 0;
+    int Count = 0;
+    Node* Popped;
+    LinkedListStack* Stack;
+    LLS_CreateStack(&Stack);
+    LLS_Push(Stack, LLS_CreateNode("abc"));
+    LLS_Push(Stack, LLS_CreateNode("def"));
+    LLS_Push(Stack, LLS_CreateNode("ghi"));
+    LLS_Push(Stack, LLS_CreateNode("jkl"));
+
+    Count = LLS_GetSize(Stack);
+    printf("Size : %d, Top : %s\n\n", Count, LLS_Top(Stack)->Data);
+
+    for (i = 0; i < Count; i++)
+    {
+        if (LLS_IsEmpty(Stack))
+            break;
+        Popped = LLS_Pop(Stack);
+        printf("Popped : %s, ", Popped->Data);
+        LLS_DestroyNode(Popped);
+        if (!LLS_IsEmpty(Stack))
+        {
+            printf("Current Top : %s\n", LLS_Top(Stack)->Data);
+        }
+        else
+        {
+            printf("Stack Is Empty.\n");
+        }
+    }
+    LLS_DestroyStack(Stack);
+    return 0;
+}
