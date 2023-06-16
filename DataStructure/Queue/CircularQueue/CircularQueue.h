6c6c092d4aab5df828fb65cb75f792bda711595a
seongjin-an <seongjin4us@gmail.com>
2023-06-16 23:54:32 +0900

    queue

---
+++ DataStructure/Queue/CircularQueue/CircularQueue.h
@@ -0,0 +1,30 @@
+#ifndef CIRCULAR_QUEUE_H_
+#define CIRCULAR_QUEUE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagNode {
+    ElementType Data;
+} Node;
+
+typedef struct tagCircularQueue
+{
+    int Capacity;
+    int Front;
+    int Rear;
+
+    Node* Nodes;
+} CircularQueue;
+
+void CQ_CreateQueue(CircularQueue** Queue, int Capacity);
+void CQ_DestroyQueue(CircularQueue* Queue);
+void CQ_Enqueue(CircularQueue*Queue, ElementType Data);
+ElementType CQ_Dequeue(CircularQueue* Queue);
+int CQ_GetSize(CircularQueue* Queue);
+int CQ_IsEmpty(CircularQueue* Queue);
+int CQ_IsFull(CircularQueue* Queue);
+
+#endif
