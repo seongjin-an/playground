54052e794c692d3367935ec7445e5733f2855ce9
anseongjin <seongjin4us@gmail.com>
2023-06-26 23:34:17 +0900

    LeftChildRightSiblingTree

---
+++ DataStructure/Tree/LCRSTree.h
@@ -0,0 +1,23 @@
+#ifndef LCRS_TREE_H_
+#define LCRS_TREE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+
+typedef struct tagLCRSNode 
+{
+    struct tagLCRSNode* LeftChild;
+    struct tagLCRSNode* RightSibling;
+
+    ElementType Data;
+} LCRSNode;
+
+LCRSNode* LCRS_CreateNode(ElementType NewData);
+void LCRS_DestroyNode(LCRSNode* Node);
+void LCRS_DestroyTree(LCRSNode* Root);
+void LCRS_AddChildNode(LCRSNode* ParentNode, LCRSNode* ChildNode);
+void LCRS_PrintTree(LCRSNode* Node, int Depth);
+
+#endif
