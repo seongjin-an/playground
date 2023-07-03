40498be37c93f2837eaf117efc424dc5529bb38c
seongjin-an <seongjin4us@gmail.com>
2023-07-03 21:46:23 +0900

    binary tree

--- DataStructure/Tree/BinaryTree/BinaryTree.h
+++ DataStructure/Tree/BinaryTree/BinaryTree.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREE_H_
 #define BINARY_TREE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef char ElementType;
 
 typedef struct tagSBTNode
@@ -13,10 +16,15 @@ typedef struct tagSBTNode
 
 SBTNode* SBT_CreateNode(ElementType NewData);
 void SBT_DestroyNode(SBTNode* Node);
-void SBT_DestroyNode(SBTNode* Root);
+void SBT_DestroyTree(SBTNode* Root);
 
 void SBT_PreorderPrintTree(SBTNode* Node);
 void SBT_InorderPrintTree(SBTNode* Node);
 void SBT_PostorderPrintTree(SBTNode* Node);
 
 #endif
+/*
+Preorder(전위 순회) : 뿌리 -> 왼쪽하위 -> 오른쪽하위
+Inorder(중위 순회) : 왼쪽하위 -> 뿌리 -> 오른쪽하위
+Postorder(후위 순회) : 왼쪽하위 -> 오른쪽하위 -> 뿌리
+*/
