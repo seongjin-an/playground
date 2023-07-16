0b48a512918c8fde16d8f5dfef93423557286434
seongjin-an <seongjin4us@gmail.com>
2023-07-17 00:11:42 +0900

    expression tree

---
+++ DataStructure/Tree/ExpressionTree/Test_ExpressionTree.c
@@ -0,0 +1,28 @@
+#include "ExpressionTree.h"
+
+int main(void)
+{
+    ETNode* Root= NULL;
+
+    char PostfixExpression[20] = "71*52-/";
+    ET_BuildExpressionTree(PostfixExpression, &Root);
+
+    //트리 출력
+    printf("Preorder ...\n");
+    ET_PreorderPrintTree(Root);
+    printf("\n\n");
+
+    printf("Inorder ...\n");
+    ET_InorderPrintTree(Root);
+    printf("\n\n");
+
+    printf("Postorder ...\n");
+    ET_PostorderPrintTree(Root);
+    printf("\n");
+
+    printf("Evaluation Result: %f \n", ET_Evaluate(Root));
+
+    // 트리소멸
+    ET_DestroyTree(Root);
+    return 0;
+}
