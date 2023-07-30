983d80f36e9b06e3a861168bc25ef8bda832f718
seongjin-an <seongjin4us@gmail.com>
2023-07-30 18:25:04 +0900

    how to build c using makefile

---
+++ DataStructure/HowToBuild/include/foo.h
@@ -0,0 +1,9 @@
+#ifndef FOO_H_
+#define FOO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintTest();
+
+#endif
---
+++ DataStructure/HowToBuild/src/foo.c
@@ -0,0 +1,6 @@
+#include "foo.h"
+
+void PrintTest() 
+{
+    printf("hello world!\n");
+}
